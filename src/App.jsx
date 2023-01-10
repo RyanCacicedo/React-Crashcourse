0fa837592b71f84835e201a02ad342bab694bed1
RyanCacicedo <Ryancacicedo@gmail.com>
2023-01-10 13:57:13 -0500

    React Intro & Reusable Components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx"
+
+function App() {
+  return (
+    <div>
+      <Title></Title>
+      <div className="todo__wrapper">
+        <Todo></Todo>
+        <Todo></Todo>
+        <Todo></Todo>
+      </div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+export default App;
