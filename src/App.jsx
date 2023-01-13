cf34bdc4b575f0b252a83714714c516b4bd3efa1
RyanCacicedo <Ryancacicedo@gmail.com>
2023-01-13 12:30:39 -0500

    Working on Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx"
+import Modal from "./components/Modal.jsx";
 
 function App() {
   return (
     <div>
       <Title></Title>
       <div className="todo__wrapper">
-        <Todo></Todo>
-        <Todo></Todo>
-        <Todo></Todo>
+        <Todo
+          title="Finish Frontend Simplified"
+          paragraph="Code along with Frontend Simplified step by step."
+        ></Todo>
+        <Todo
+          title="Finish Interview Section"
+          paragraph="Finish every interview question in the next 6 weeks."
+        ></Todo>
+        <Todo
+          title="Land a $100k Job"
+          paragraph="Apply to 100 jobs."
+        ></Todo>
       </div>
-      <Modal></Modal>
+      <Modal title="Are you sure you want to delete?"></Modal>
     </div>
   );
 }
