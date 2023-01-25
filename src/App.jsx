d11660495d8a598c4c49efcff7549c6000e7c321
RyanCacicedo <Ryancacicedo@gmail.com>
2023-01-25 17:11:57 -0500

    Api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home.jsx'
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,10 +9,9 @@ import Users from "./pages/Users";
 function App() {
   return (
     <Router>
-     <Nav></Nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/users/:username" element={<Users />}></Route>
+        <Route path="/users/:id" element={<Users />}></Route>
       </Routes>
     </Router>
   );
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,30 @@
 import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react"
+import axios from 'axios'
+import { Link } from "react-router-dom";
 
 function Users() {
-    const { username } = useParams()
+    const { id } = useParams()
+    const [user, setUser] = useState({})
 
-    return <h1>{username}</h1>
+    async function fetchUser() {
+    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    setUser(data)
+    }
+
+    useEffect(() => {
+        fetchUser()
+}, [])
+    
+    return (
+        <div>
+            <Link to="/">Go Back</Link>
+            <p>{ user.id }</p>
+            <p>{ user.name }</p>
+            <p>{ user.email }</p>
+            <p>{ user.username }</p>
+        </div>
+    )
 }
 
 export default Users
